a093ee3330a44169830b20d5d3ea0408a50d863c
Юрий Кузнецов <omega131995@outlook.com>
2024-08-01 23:22:25 +0600

    Заготовка под верхнее меню

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,17 @@ import './App.css'
 function App() {
 
   return (
-    <>
-
-    </>
+    <div id="top_menu">
+      <div>
+        <div className="menu-item">Элемент 1</div>
+        <div className="menu-item">Элемент 2</div>
+        <div className="menu-item">Элемент 3</div>
+        <div className="menu-item">Элемент 4</div>
+      </div>
+      <div>
+        Аккаунт
+      </div>
+    </div>
   )
 }
 
