655708fba7be390749cb14ae5f00f5a20edcbc9b
Юрий Кузнецов <omega131995@outlook.com>
2024-08-02 07:36:41 +0600

    Добавил микрофронтенд каталога приложений

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import './App.css'
+import AppCategories from "app_catalog/AppCategories"
 
 function App() {
 
-  return (
-    <div id="top_menu">
-      <div>
-        <div className="menu-item">Элемент 1</div>
-        <div className="menu-item">Элемент 2</div>
-        <div className="menu-item">Элемент 3</div>
-        <div className="menu-item">Элемент 4</div>
+  return <>
+      <div id="top_menu">
+          <div>
+              <div className="menu-item">Элемент 1</div>
+              <div className="menu-item">Элемент 2</div>
+              <div className="menu-item">Элемент 3</div>
+              <div className="menu-item">Элемент 4</div>
+          </div>
+          <div>
+              Аккаунт
+          </div>
       </div>
       <div>
-        Аккаунт
+          <AppCategories/>
       </div>
-    </div>
-  )
+  </>
+
 }
 
 export default App
