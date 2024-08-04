bf7f703e57f26380b3cc718b7044a67a357971fb
Юрий Кузнецов <omega131995@outlook.com>
2024-08-04 08:06:15 +0600

    Реализация монорепозитория

---
+++ vite.dev.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation";
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	preview:{
+		port: 80
+	},
+    plugins: [
+      react(),
+      federation({
+        name: "container",
+        remotes: {
+            app_catalog: "http://localhost:81/assets/remoteEntry.js"
+        },
+        shared: [
+            "react"
+        ]
+      })
+    ],
+    build: {
+        target: "esnext",
+    }
+})
